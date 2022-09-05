fa97119b67536320d68b93019c53274dbd6070eb
wouterlansu <wouter@stitch3d.com>
2022-09-05 19:50:38 +0200

    Cleanup

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,5 @@
 #!/bin/sh
 
-if [ -z "${POSTGRES_USER}" ]; then
-    base_postgres_image_default_user='postgres'
-    export POSTGRES_USER="${base_postgres_image_default_user}"
-fi
-export DATABASE_URL="postgres://${POSTGRES_USER}:${POSTGRES_PASSWORD}@${POSTGRES_HOST}:${POSTGRES_PORT}/${POSTGRES_DB}"
-
 echo "Running Django migrations"
 python manage.py makemigrations --noinput --verbosity 0
 if [ $? -eq 3 ]
