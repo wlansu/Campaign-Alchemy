4711f9132bb43a9934909d4a09f965608e829ca2
Wouter Lansu <wfrlansu@gmail.com>
2022-10-10 10:43:38 +0200

    Add boto3 and do spaces info

--- campaignalchemy/settings/production.py
+++ campaignalchemy/settings/production.py
@@ -71,6 +71,14 @@ STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 # MEDIA
 # ------------------------------------------------------------------------------
 DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
+AWS_S3_REGION_NAME = "ams3"
+AWS_S3_ENDPOINT_URL = "https://ams3.digitaloceanspaces.com"
+AWS_ACCESS_KEY_ID = env("AWS_ACCESS_KEY_ID")
+AWS_SECRET_ACCESS_KEY = env("AWS_SECRET_ACCESS_KEY")
+AWS_STORAGE_BUCKET_NAME = "campaign-alchemy"
+AWS_DEFAULT_ACL = "public-read"
+AWS_S3_OBJECT_PARAMETERS = {"CacheControl": "max-age=86400"}
+AWS_LOCATION = "static"
 
 # EMAIL
 # ------------------------------------------------------------------------------
