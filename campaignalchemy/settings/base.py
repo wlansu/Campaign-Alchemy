7a6c99c2cf49618a7daa86da766bafafa888acb4
Wouter Lansu <wfrlansu@gmail.com>
2022-10-15 22:28:24 +0200

    Align configuration of tinymce and fix overflow issue on map detail

--- campaignalchemy/settings/base.py
+++ campaignalchemy/settings/base.py
@@ -290,3 +290,12 @@ AWS_SECRET_ACCESS_KEY = env("AWS_SECRET_ACCESS_KEY", default=None)
 AWS_STORAGE_BUCKET_NAME = env("AWS_STORAGE_BUCKET_NAME", default=None)
 AWS_DEFAULT_ACL = "public-read"
 # ------------------------------------------------------------------------------
+
+# TinyMCE
+TINYMCE_DEFAULT_CONFIG = {
+    "theme": "silver",
+    "height": 500,
+    "menubar": False,
+    "plugins": "lists,link",
+    "toolbar": "styleselect | forecolor | bold italic | link | bullist numlist",
+}
