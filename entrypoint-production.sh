712d61d7d2ab0a3ccb77d2519922147c45ced365
Wouter Lansu <3016789+wlansu@users.noreply.github.com>
2022-11-04 11:35:28 +0100

    Add more workers

--- entrypoint-production.sh
+++ entrypoint-production.sh
@@ -18,4 +18,4 @@ python manage.py collectstatic --noinput --verbosity 0
 echo "Compressing files"
 python manage.py compress
 
-gunicorn campaignalchemy.wsgi:application --bind 0.0.0.0:8000
+gunicorn campaignalchemy.wsgi:application --bind 0.0.0.0:8000 --workers 3
