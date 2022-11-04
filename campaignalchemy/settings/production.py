f103bff6d4741c0c6e41a690e7a76ef5ee921d58
Wouter Lansu <3016789+wlansu@users.noreply.github.com>
2022-11-04 21:13:04 +0100

    Fix wrong email backend

--- campaignalchemy/settings/production.py
+++ campaignalchemy/settings/production.py
@@ -106,20 +106,6 @@ ANYMAIL = {
 # Django Admin URL regex.
 ADMIN_URL = "admin/"
 
-# Anymail
-# ------------------------------------------------------------------------------
-# https://anymail.readthedocs.io/en/stable/installation/#installing-anymail
-# INSTALLED_APPS += ["anymail"]  # noqa F405
-# https://docs.djangoproject.com/en/dev/ref/settings/#email-backend
-# https://anymail.readthedocs.io/en/stable/installation/#anymail-settings-reference
-# https://anymail.readthedocs.io/en/stable/esps/mailgun/
-EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
-# ANYMAIL = {
-#     "MAILGUN_API_KEY": env("MAILGUN_API_KEY"),
-#     "MAILGUN_SENDER_DOMAIN": env("MAILGUN_DOMAIN"),
-#     "MAILGUN_API_URL": env("MAILGUN_API_URL", default="https://api.mailgun.net/v3"),
-# }
-
 # django-compressor
 # ------------------------------------------------------------------------------
 # https://django-compressor.readthedocs.io/en/latest/settings/#django.conf.settings.COMPRESS_ENABLED
