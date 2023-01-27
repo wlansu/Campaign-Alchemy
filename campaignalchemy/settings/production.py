25a20368ae9b24e096a29758e3c50e897bf2d399
Wouter Lansu <wlansu@users.noreply.github.com>
2023-01-27 22:10:00 +0100

    Enable cdn

--- campaignalchemy/settings/production.py
+++ campaignalchemy/settings/production.py
@@ -73,7 +73,7 @@ STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 # ------------------------------------------------------------------------------
 DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
 AWS_S3_REGION_NAME = "ams3"
-AWS_S3_ENDPOINT_URL = "https://ams3.digitaloceanspaces.com"
+AWS_S3_ENDPOINT_URL = "https://ams3.cdn.digitaloceanspaces.com"
 AWS_ACCESS_KEY_ID = env("AWS_ACCESS_KEY_ID")
 AWS_SECRET_ACCESS_KEY = env("AWS_SECRET_ACCESS_KEY")
 AWS_STORAGE_BUCKET_NAME = "campaign-alchemy"
