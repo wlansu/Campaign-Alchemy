4f17f7894d034b4ef25ad5a86b21c32fecc855b2
Wouter Lansu <wlansu@users.noreply.github.com>
2023-01-27 22:38:45 +0100

    Add s3_custom_domain

--- campaignalchemy/settings/production.py
+++ campaignalchemy/settings/production.py
@@ -73,7 +73,8 @@ STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 # ------------------------------------------------------------------------------
 DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
 AWS_S3_REGION_NAME = "ams3"
-AWS_S3_ENDPOINT_URL = "campaign-alchemy.ams3.cdn.digitaloceanspaces.com"
+AWS_S3_ENDPOINT_URL = "https://campaign-alchemy.ams3.digitaloceanspaces.com"
+AWS_S3_CUSTOM_DOMAIN = "campaign-alchemy.ams3.cdn.digitaloceanspaces.com"
 AWS_ACCESS_KEY_ID = env("AWS_ACCESS_KEY_ID")
 AWS_SECRET_ACCESS_KEY = env("AWS_SECRET_ACCESS_KEY")
 AWS_STORAGE_BUCKET_NAME = "campaign-alchemy"
